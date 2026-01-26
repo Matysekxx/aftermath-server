a8349dcec3099b4931cbae72e57da558fd92fc62
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-26 17:23:52 +0100

    Add StationaryBehavior

--- src/main/java/cz/matysekxx/aftermathserver/core/model/behavior/StationaryBehavior.java
+++ src/main/java/cz/matysekxx/aftermathserver/core/model/behavior/StationaryBehavior.java
@@ -1,12 +1,14 @@
 package cz.matysekxx.aftermathserver.core.model.behavior;
 
 import cz.matysekxx.aftermathserver.core.model.entity.Npc;
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
 import cz.matysekxx.aftermathserver.core.world.GameMapData;
+
+import java.util.Collection;
 
 /// Behavior for NPCs that stay in one place (e.g., guards, shopkeepers).
 public class StationaryBehavior implements Behavior {
     @Override
-    public void performAction(Npc npc, GameMapData gameMapData) {
-
+    public void update(Npc npc, GameMapData map, Collection<Player> players) {
     }
 }
