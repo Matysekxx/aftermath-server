185068b1e279b2dd171ad13e3e85a4260d444b97
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-26 17:24:27 +0100

    Add PatrolBehavior class without logic

--- src/main/java/cz/matysekxx/aftermathserver/core/model/behavior/PatrolBehavior.java
+++ src/main/java/cz/matysekxx/aftermathserver/core/model/behavior/PatrolBehavior.java
@@ -1,12 +1,15 @@
 package cz.matysekxx.aftermathserver.core.model.behavior;
 
 import cz.matysekxx.aftermathserver.core.model.entity.Npc;
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
 import cz.matysekxx.aftermathserver.core.world.GameMapData;
+
+import java.util.Collection;
 
 /// Behavior for NPCs that patrol between points.
 public class PatrolBehavior implements Behavior {
     @Override
-    public void performAction(Npc npc, GameMapData gameMapData) {
-
+    public void update(Npc npc, GameMapData map, Collection<Player> players) {
+        // TODO: Implement patrol logic (moving between predefined waypoints)
     }
 }
