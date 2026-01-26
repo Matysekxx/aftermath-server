73689bea05536d815e725f23f90ba61840afcecc
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-26 18:50:35 +0100

    Fix TileRegistry configuration from yml

--- src/main/java/cz/matysekxx/aftermathserver/core/world/TileRegistry.java
+++ src/main/java/cz/matysekxx/aftermathserver/core/world/TileRegistry.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /// Registry mapping characters in map files to `TileType` definitions.
@@ -16,7 +17,7 @@ import java.util.Map;
 @Configuration
 @ConfigurationProperties(prefix = "game.tiles")
 public class TileRegistry {
-    private Collection<TileDefinition> definitions;
+    private List<TileDefinition> definitions;
     private Map<Character, TileType> mapping = new HashMap<>();
 
     @PostConstruct
