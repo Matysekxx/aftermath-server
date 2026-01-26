5c15de16b63252745eeb0cb624251d35e7d7b270
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-26 19:39:35 +0100

    Add ItemTemplate

---
+++ src/main/java/cz/matysekxx/aftermathserver/core/model/item/ItemTemplate.java
@@ -0,0 +1,19 @@
+package cz.matysekxx.aftermathserver.core.model.item;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/// Represents the static definition of an item loaded from configuration.
+@Data
+@NoArgsConstructor
+public class ItemTemplate {
+    private String id;
+    private String name;
+    private String description;
+    private int maxStack;
+    private double weight;
+    private ItemType type;
+    private Integer healAmount;
+    private Integer damage;
+    private Integer price;
+}
