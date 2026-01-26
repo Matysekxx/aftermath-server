f75804700ff82c6a8587f294c6e9273a6544cb9f
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-26 17:28:08 +0100

    Add AggressiveBehavior

--- src/main/java/cz/matysekxx/aftermathserver/core/model/behavior/AggressiveBehavior.java
+++ src/main/java/cz/matysekxx/aftermathserver/core/model/behavior/AggressiveBehavior.java
@@ -1,12 +1,40 @@
 package cz.matysekxx.aftermathserver.core.model.behavior;
 
 import cz.matysekxx.aftermathserver.core.model.entity.Npc;
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
 import cz.matysekxx.aftermathserver.core.world.GameMapData;
+import cz.matysekxx.aftermathserver.util.MathUtil;
+import cz.matysekxx.aftermathserver.util.Vector2;
 
-/// Behavior for hostile NPCs that attack players.
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.function.Consumer;
+
+/// Hostile behavior that chases and attacks the nearest player.
 public class AggressiveBehavior implements Behavior {
-    @Override
-    public void performAction(Npc npc, GameMapData gameMapData) {
+    private final int visionRange = 10;
 
+    @Override
+    public void update(Npc npc, GameMapData map, Collection<Player> players) {
+        final Vector2 npcPos = new Vector2(npc.getX(), npc.getY());
+
+        players.stream()
+                .filter(p -> !p.isDead())
+                .filter(p -> MathUtil.getChebyshevDistance(npcPos, new Vector2(p.getX(), p.getY())) <= visionRange)
+                .min(Comparator.comparingInt(p -> MathUtil.getChebyshevDistance(npcPos, new Vector2(p.getX(), p.getY()))))
+                .ifPresent(target -> {
+                    final int distance = MathUtil.getChebyshevDistance(npcPos, new Vector2(target.getX(), target.getY()));
+                    if (distance <= 1) {
+                        attack(npc, target);
+                    } else {
+                        chase(npc, target, map);
+                    }
+                });
+    }
+
+    private void attack(Npc npc, Player target) {
+    }
+
+    private void chase(Npc npc, Player target, GameMapData map) {
     }
 }
