ad7ea4016a1e5c47705b4f59ec6f49ed12df0442
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-29 19:51:19 +0100

    Add class Quadtree without logic

---
+++ src/main/java/cz/matysekxx/aftermathserver/util/QuadTree.java
@@ -0,0 +1,18 @@
+package cz.matysekxx.aftermathserver.util;
+
+import cz.matysekxx.aftermathserver.core.model.entity.Entity;
+
+public class QuadTree {
+    private QuadTree[] nodes;
+    public QuadTree() {
+
+    }
+
+    public void insert(Entity entity) {
+
+    }
+
+    public void clear() {
+
+    }
+}
