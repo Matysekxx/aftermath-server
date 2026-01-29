4cbcd0f8ec60d6432abc3337abf4858b2343afdd
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-29 20:09:37 +0100

    Add UseAction

--- src/main/java/cz/matysekxx/aftermathserver/action/UseAction.java
+++ src/main/java/cz/matysekxx/aftermathserver/action/UseAction.java
@@ -1,8 +1,23 @@
 package cz.matysekxx.aftermathserver.action;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import cz.matysekxx.aftermathserver.core.GameEngine;
+import cz.matysekxx.aftermathserver.dto.UseRequest;
+import org.springframework.stereotype.Component;
+
 /// Handles using consumable items.
 ///
 /// Triggered by the `USE` command.
 /// Applies the effect of a consumable item (e.g., healing, radiation reduction) to the player.
-public class UseAction {
+@Component("USE")
+public class UseAction extends Action {
+    protected UseAction(GameEngine gameEngine) {
+        super(gameEngine);
+    }
+
+    @Override
+    public void execute(String sessionId, JsonNode payload) {
+        final UseRequest useRequest = objectMapper.convertValue(payload, UseRequest.class);
+        gameEngine.processUse(sessionId, useRequest);
+    }
 }
