53c7cf43acf4bad7b8e185996fa27e6051e5042c
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-29 20:09:26 +0100

    Add AttackAction

--- src/main/java/cz/matysekxx/aftermathserver/action/AttackAction.java
+++ src/main/java/cz/matysekxx/aftermathserver/action/AttackAction.java
@@ -1,8 +1,23 @@
 package cz.matysekxx.aftermathserver.action;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import cz.matysekxx.aftermathserver.core.GameEngine;
+import cz.matysekxx.aftermathserver.dto.AttackRequest;
+import org.springframework.stereotype.Component;
+
 /// Handles combat attacks.
 ///
 /// Triggered by the `ATTACK` command.
 /// Calculates damage and applies it to the target entity.
-public class AttackAction {
+@Component("ATTACK")
+public class AttackAction extends Action {
+    protected AttackAction(GameEngine gameEngine) {
+        super(gameEngine);
+    }
+
+    @Override
+    public void execute(String sessionId, JsonNode payload) {
+        final AttackRequest attackRequest = objectMapper.convertValue(payload, AttackRequest.class);
+        gameEngine.processAttack(sessionId, attackRequest);
+    }
 }
