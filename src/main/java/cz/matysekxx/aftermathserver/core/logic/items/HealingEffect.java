bc0071fbb4f8900f8de67eec8c8e7ef23228b56d
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-29 21:51:29 +0100

    Add Healing Effect

---
+++ src/main/java/cz/matysekxx/aftermathserver/core/logic/items/HealingEffect.java
@@ -0,0 +1,17 @@
+package cz.matysekxx.aftermathserver.core.logic.items;
+
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
+import cz.matysekxx.aftermathserver.core.model.item.Item;
+import org.springframework.stereotype.Component;
+
+/// Logic for items that restore health (Medkits, Food).
+@Component("HEAL")
+public class HealingEffect implements ItemEffect {
+    @Override
+    public boolean apply(Player player, Item item) {
+        if (player.getHp() >= player.getMaxHp()) return false;
+        final int amount = item.getHealAmount() != null ? item.getHealAmount() : 0;
+        player.setHp(Math.min(player.getMaxHp(), player.getHp() + amount));
+        return true;
+    }
+}
