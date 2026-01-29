d837bcb12c1ca26764274c04a2fc350e35595dfb
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-29 21:50:18 +0100

    Add ItemEffect interface

---
+++ src/main/java/cz/matysekxx/aftermathserver/core/logic/items/ItemEffect.java
@@ -0,0 +1,14 @@
+package cz.matysekxx.aftermathserver.core.logic.items;
+
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
+import cz.matysekxx.aftermathserver.core.model.item.Item;
+
+/// Interface for defining the behavior of usable items.
+public interface ItemEffect {
+    /// Applies the effect of the item to the player.
+    ///
+    /// @param player The player using the item.
+    /// @param item The item instance being consumed.
+    /// @return true if the effect was applied successfully.
+    boolean apply(Player player, Item item);
+}
