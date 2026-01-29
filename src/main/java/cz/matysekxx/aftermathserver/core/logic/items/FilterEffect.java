32b7abc329b93e23f1a360ebea2a88add91f201a
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-29 21:51:56 +0100

    Add FilterEffect

---
+++ src/main/java/cz/matysekxx/aftermathserver/core/logic/items/FilterEffect.java
@@ -0,0 +1,16 @@
+package cz.matysekxx.aftermathserver.core.logic.items;
+
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
+import cz.matysekxx.aftermathserver.core.model.item.Item;
+import org.springframework.stereotype.Component;
+
+/// Logic for items that increase the player's radiation limit (Filters).
+@Component("FILTER")
+public class FilterEffect implements ItemEffect {
+    @Override
+    public boolean apply(Player player, Item item) {
+        final int increase = item.getHealAmount() != null ? item.getHealAmount() : 50;
+        player.setRadsLimit(player.getRadsLimit() + increase);
+        return true;
+    }
+}
