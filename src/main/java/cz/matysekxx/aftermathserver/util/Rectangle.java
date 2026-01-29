945aa04429f1bf5d9a0d66f31ff98272045ae7c4
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-29 19:48:40 +0100

    Add Rectangle record class

---
+++ src/main/java/cz/matysekxx/aftermathserver/util/Rectangle.java
@@ -0,0 +1,8 @@
+package cz.matysekxx.aftermathserver.util;
+
+
+/// @param x Rectangle top-left corner position x
+/// @param y Rectangle top-left corner position y
+/// @param w Rectangle width
+/// @param h Rectangle height
+public record Rectangle(int x, int y, int w, int h) {}
