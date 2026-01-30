1e11f2786b97012ef6a775a55aaecebef89e6296
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-30 22:40:08 +0100

    Add EquipRequest

---
+++ src/main/java/cz/matysekxx/aftermathserver/dto/EquipRequest.java
@@ -0,0 +1,10 @@
+package cz.matysekxx.aftermathserver.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EquipRequest {
+    private int slotIndex;
+}
