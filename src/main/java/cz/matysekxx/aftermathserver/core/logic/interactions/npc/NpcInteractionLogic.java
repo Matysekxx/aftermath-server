3ebcb21ca262d6c6b96d9b335d4f0a48b33ac6f2
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-30 23:34:23 +0100

    Add NpcInteractionLogic interface

---
+++ src/main/java/cz/matysekxx/aftermathserver/core/logic/interactions/npc/NpcInteractionLogic.java
@@ -0,0 +1,12 @@
+package cz.matysekxx.aftermathserver.core.logic.interactions.npc;
+
+import cz.matysekxx.aftermathserver.core.model.entity.Npc;
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
+import cz.matysekxx.aftermathserver.event.GameEvent;
+
+import java.util.Collection;
+
+public interface NpcInteractionLogic {
+
+    Collection<GameEvent> interact(Npc target, Player player);
+}
