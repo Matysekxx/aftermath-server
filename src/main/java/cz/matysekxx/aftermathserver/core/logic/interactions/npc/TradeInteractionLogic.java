c2c4f3dc5549489c7c50a9aa7f664705bc41c7c8
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-31 12:28:49 +0100

    Add interaction logic classes for npc interaction

---
+++ src/main/java/cz/matysekxx/aftermathserver/core/logic/interactions/npc/TradeInteractionLogic.java
@@ -0,0 +1,23 @@
+package cz.matysekxx.aftermathserver.core.logic.interactions.npc;
+
+import cz.matysekxx.aftermathserver.core.model.entity.InteractionType;
+import cz.matysekxx.aftermathserver.core.model.entity.Npc;
+import cz.matysekxx.aftermathserver.core.model.entity.Player;
+import cz.matysekxx.aftermathserver.event.GameEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class TradeInteractionLogic implements NpcInteractionLogic {
+    @Override
+    public Collection<GameEvent> interact(Npc target, Player player) {
+        return List.of();
+    }
+
+    @Override
+    public InteractionType getType() {
+        return InteractionType.TRADE;
+    }
+}
