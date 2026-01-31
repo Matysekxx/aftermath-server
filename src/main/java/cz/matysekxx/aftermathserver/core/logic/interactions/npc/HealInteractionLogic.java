0dc7ec50e4f745826319db5f5595afeba8f8a4fa
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-31 13:00:34 +0100

    Add logic to HealInteractionLogic

--- src/main/java/cz/matysekxx/aftermathserver/core/logic/interactions/npc/HealInteractionLogic.java
+++ src/main/java/cz/matysekxx/aftermathserver/core/logic/interactions/npc/HealInteractionLogic.java
@@ -4,6 +4,7 @@ import cz.matysekxx.aftermathserver.core.model.entity.InteractionType;
 import cz.matysekxx.aftermathserver.core.model.entity.Npc;
 import cz.matysekxx.aftermathserver.core.model.entity.Player;
 import cz.matysekxx.aftermathserver.event.GameEvent;
+import cz.matysekxx.aftermathserver.event.GameEventFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -13,7 +14,21 @@ import java.util.List;
 public class HealInteractionLogic implements NpcInteractionLogic {
     @Override
     public Collection<GameEvent> interact(Npc target, Player player) {
-        return List.of();
+        final int cost = 20;
+        if (player.getHp() < player.getMaxHp() || player.getRads() > 0){
+            if (player.getCredits() < cost) {
+                return List.of(GameEventFactory.sendErrorEvent("You don't have enough credits! Price: " + cost, player.getId()));
+            }
+            player.setRads(0);
+            player.setHp(player.getMaxHp());
+            player.removeCredits(cost);
+            return List.of(
+                    GameEventFactory.sendMessageEvent("You are now fully healed", player.getId()),
+                    GameEventFactory.sendStatsEvent(player)
+            );
+        } else {
+            return List.of(GameEventFactory.sendMessageEvent("You are fully healed you don't need to heal", player.getId()));
+        }
     }
 
     @Override
